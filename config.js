1b1a8982d42bdbb3bc2775ba8e5bf1e4b1f85cd6
din123esh <bagheldinesh46734@gmail.com>
2025-05-28 15:45:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const parsedId = Number(process.env.ID);
 module.exports = {
-  token: process.env.TOKEN || 'YOUR_BOT_TOKEN_HERE',
-  id: isNaN(parsedId) ? 123456789 : parsedId // replace 12345.. with your telegram chat id
+  token: process.env.TOKEN || 'https://github.com/din123esh/TELE-RAT1/tree/main',
+  id: isNaN(parsedId) ? 1603022382 : parsedId // replace 12345.. with your telegram chat id
 };
